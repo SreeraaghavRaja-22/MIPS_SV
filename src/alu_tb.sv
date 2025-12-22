ee9b3dc6b2dfff6e7c3beee6ffc2f19ed9c0bb1c
SreeraaghavRaja-22 <rajas@ufl.edu>
2025-12-21 23:11:09 -0500

    fixed incomplete tb

---
+++ src/alu_tb.sv
@@ -0,0 +1,21 @@
+`timescale 1us/1ns
+module alu_tb(); 
+
+    // All TB inputs
+    localparam int WIDTH = 32;
+
+    logic [WIDTH-1:0] reg_a, reg_b; 
+    logic [4:0] opsel, ir_shift;
+    logic [WIDTH-1:0] result, result_hi; 
+    logic branch_taken; 
+
+
+    function [2**WIDTH:0] check_flags (input [WIDTH-1:0] reg_a_in, input [WIDTH-1:0] reg_b_in, input [4:0] opsel, input [4:0] ir_shift)
+        begin 
+        end 
+    endfunction
+
+    initial begin 
+        $timeformat(-9, 0, " ns");
+    end
+        
